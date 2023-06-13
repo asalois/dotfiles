30854cb46a315ad124f339a709c9755984db6493
Alex Salois <asalois.scholar@gmail.com>
2023-06-12 19:52:57 -0600

    replaced with link.sh

--- sync.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-rsync -avP --progress dot_zshrc ~/.zshrc
-rsync -rvP --progress starhsip.toml ~/.config/
-rsync -rvP --progress dot_vimrc ~/.vimrc
-rsync -rvP --progress my-theme.zsh-theme ~/.oh-my-zsh/themes/
-rsync -rvP --progress ssh_config ~/.ssh/config
-chmod 400 ~/.ssh/config
