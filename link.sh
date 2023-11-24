6f52bb9c4a93291d065845f5955f9e6439c4328b
Alex Salois <asalois.scholar@gmail.com>
2023-11-24 11:43:37 -0700

    added link to config.fish

--- link.sh
+++ link.sh
@@ -2,6 +2,7 @@
 dir=$(pwd)
 ln -s  $dir/dot_zshrc ~/.zshrc
 ln -s  $dir/dot_bashrc ~/.bashrc
+ln -s  $dir/config.fish ~/.config/fish/config.fish
 ln -s  $dir/starship.toml ~/.config/starship.toml
 ln -s  $dir/dot_vimrc ~/.vimrc
 ln -s  $dir/ssh_config ~/.ssh/config
