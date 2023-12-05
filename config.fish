af39a2e3c901199d36168f93c56e0ae2b4380642
Alex Salois <asalois.scholar@gmail.com>
2023-12-04 18:01:41 -0700

    Switch from micomamaba to miniforge

--- config.fish
+++ config.fish
@@ -1,19 +1,25 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
+    source /home/alex/spack/share/spack/setup-env.fish
+    set PATH $PATH /home/alex/.cargo/bin
+    set BAT_THEME "Coldark-Dark"
+    set GIT_EDITOR micro
+    set EDITOR micro
+    alias update="sudo apt update && sudo apt upgrade && sudo apt autoremove && flatpak update"
+    alias cat=batcat
+    alias ls=lsd
+    starship init fish | source
 end
 
-# >>> mamba initialize >>>
-# !! Contents within this block are managed by 'mamba init' !!
-set -gx MAMBA_EXE "/home/alex/.local/bin/micromamba"
-set -gx MAMBA_ROOT_PREFIX "/home/alex/micromamba"
-$MAMBA_EXE shell hook --shell fish --root-prefix $MAMBA_ROOT_PREFIX | source
-# <<< mamba initialize <<<
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+if test -f /home/alex/miniforge3/bin/conda
+    eval /home/alex/miniforge3/bin/conda "shell.fish" "hook" $argv | source
+end
 
-source /home/alex/spack/share/spack/setup-env.fish
-set PATH $PATH /home/alex/.cargo/bin
-set BAT_THEME "Coldark-Dark"
-set GIT_EDITOR micro
-set EDITOR micro
-alias cat=batcat
-alias ls=lsd
-starship init fish | source
+if test -f "/home/alex/miniforge3/etc/fish/conf.d/mamba.fish"
+    source "/home/alex/miniforge3/etc/fish/conf.d/mamba.fish"
+end
+# <<< conda initialize <<<
+
+
