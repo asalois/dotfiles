5f175b8397b3cb78eba516cfcaaa13d4e9d8d173
Alex Salois <asalois.scholar@gmail.com>
2023-12-09 11:02:52 -0700

    Current setup on fedora

--- config.fish
+++ config.fish
@@ -1,19 +1,12 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
+    source /home/alex/spack/share/spack/setup-env.fish
+    set PATH $PATH /home/alex/.cargo/bin
+    set BAT_THEME "Coldark-Dark"
+    set GIT_EDITOR vim
+    set EDITOR vim
+    alias cat=bat
+    alias ls=lsd
+    starship init fish | source
+    fortune | cowsay -f tux
 end
-
-# >>> mamba initialize >>>
-# !! Contents within this block are managed by 'mamba init' !!
-set -gx MAMBA_EXE "/home/alex/.local/bin/micromamba"
-set -gx MAMBA_ROOT_PREFIX "/home/alex/micromamba"
-$MAMBA_EXE shell hook --shell fish --root-prefix $MAMBA_ROOT_PREFIX | source
-# <<< mamba initialize <<<
-
-source /home/alex/spack/share/spack/setup-env.fish
-set PATH $PATH /home/alex/.cargo/bin
-set BAT_THEME "Coldark-Dark"
-set GIT_EDITOR micro
-set EDITOR micro
-alias cat=batcat
-alias ls=lsd
-starship init fish | source
--- set-up.sh
+++ set-up.sh
@@ -3,23 +3,23 @@
 sudo bash -c "echo -e 'fastestmirror=True\ndeltarpm=True\nmax_parallel_downlods=10' >> /etc/dnf/dnf.conf"
 
 # update and enable repos
-sudo dnf install --nogpgcheck https://dl.fedoraproject.org/pub/epel/epel-release-latest-$(rpm -E %rhel).noarch.rpm
-sudo dnf install --nogpgcheck https://mirrors.rpmfusion.org/free/el/rpmfusion-free-release-$(rpm -E %rhel).noarch.rpm https://mirrors.rpmfusion.org/nonfree/el/rpmfusion-nonfree-release-$(rpm -E %rhel).noarch.rpm
-sudo dnf config-manager --enable crb
+#sudo dnf install --nogpgcheck https://dl.fedoraproject.org/pub/epel/epel-release-latest-$(rpm -E %rhel).noarch.rpm
+#sudo dnf install --nogpgcheck https://mirrors.rpmfusion.org/free/el/rpmfusion-free-release-$(rpm -E %rhel).noarch.rpm https://mirrors.rpmfusion.org/nonfree/el/rpmfusion-nonfree-release-$(rpm -E %rhel).noarch.rpm
+#sudo dnf config-manager --enable crb
 #sudo dnf config-manager --enable powertools
-sudo dnf update
+#sudo dnf update
 
 # install packages needed for linux brew
 sudo dnf install zsh wget curl git
 sudo dnf group install "Development Tools"
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+#/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 sleep 2
-ehco 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> .bashrc # install brew
-brew install btop htop neofetch fzf bat starhsip
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended # install oh my zsh
-sleep 2
-sudo dnf install redhat-mono-fonts google-droid-sans-mono-fonts google-noto-sans-mono-fonts powerline-fonts # install some fonts
-bash -c  "$(wget -qO- https://git.io/vQgMr)"
+#echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> .bashrc # install brew
+#brew install btop htop neofetch fzf bat starhsip
+#sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended # install oh my zsh
+#sleep 2
+#sudo dnf install redhat-mono-fonts google-droid-sans-mono-fonts google-noto-sans-mono-fonts powerline-fonts # install some fonts
+#bash -c  "$(wget -qO- https://git.io/vQgMr)"
 sleep 2
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.2.2/Hack.zip
 unzip -d hack-font Hack.zip
