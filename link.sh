e5353b974d8ae4d06e6062db48449aee7b346cb7
Alex Salois <asalois.scholar@gmail.com>
2024-01-04 23:20:46 -0700

    Added new files to make links to

--- link.sh
+++ link.sh
@@ -4,5 +4,7 @@ ln -s  $dir/dot_zshrc ~/.zshrc
 ln -s  $dir/dot_bashrc ~/.bashrc
 ln -s  $dir/config.fish ~/.config/fish/config.fish
 ln -s  $dir/starship.toml ~/.config/starship.toml
+ln -s  $dir/alacritty.yml ~/.config/alacritty/alacritty.yml
 ln -s  $dir/dot_vimrc ~/.vimrc
+ln -s  $dir/dot_gotconfig ~/.gitconfig
 ln -s  $dir/ssh_config ~/.ssh/config
