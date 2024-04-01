cb86201bf14ea36d01501e3c8b102a067f56e592
Alexander Salois <alexander.salois@montana.edu>
2024-04-01 11:46:24 -0600

    Set up a script to make links

---
+++ link.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+ln -sf $HOME/dotfiles/dot_bashrc $HOME/.bashrc
+ln -sf $HOME/dotfiles/dot_gitconfig $HOME/.gitconfig
+ln -sf $HOME/dotfiles/config.fish $HOME/.config/fish/config.fish
+
