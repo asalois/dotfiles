5f0f27770272db8b57f39bbd9b233256f585c0b8
Alex Salois <asalois.scholar@gmail.com>
2024-05-18 16:20:40 -0600

    Moved to interactive and removed mamba

--- config.fish
+++ config.fish
@@ -1,19 +1,15 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
+    #source /home/alex/spack/share/spack/setup-env.fish
+    set PATH $PATH /home/deb_win/.cargo/bin
+    set BAT_THEME "Coldark-Dark"
+    set GIT_EDITOR vim
+    set EDITOR vim
+    alias cat=batcat
+    alias ls=lsd
+    alias update="sudo apt update && sudo apt upgrade && sudo apt autoremove"
+    alias tempest="ssh v16b915@tempest-login.msu.montana.edu"
+    starship init fish | source
+    fortune | cowsay -f tux
 end
 
-# >>> mamba initialize >>>
-# !! Contents within this block are managed by 'mamba init' !!
-set -gx MAMBA_EXE "/home/alex/.local/bin/micromamba"
-set -gx MAMBA_ROOT_PREFIX "/home/alex/micromamba"
-$MAMBA_EXE shell hook --shell fish --root-prefix $MAMBA_ROOT_PREFIX | source
-# <<< mamba initialize <<<
-
-source /home/alex/spack/share/spack/setup-env.fish
-set PATH $PATH /home/alex/.cargo/bin
-set BAT_THEME "Coldark-Dark"
-set GIT_EDITOR micro
-set EDITOR micro
-alias cat=batcat
-alias ls=lsd
-starship init fish | source
