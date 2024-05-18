9e7501777a66e00fb14b8f1a47eda37ee5706f25
Alex Salois <asalois.scholar@gmail.com>
2024-05-18 17:32:26 -0600

    Added themes to be linked

--- link.sh
+++ link.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 dir=$(pwd)
-ln -s  $dir/dot_zshrc ~/.zshrc
-ln -s  $dir/dot_bashrc ~/.bashrc
-ln -s  $dir/config.fish ~/.config/fish/config.fish
-ln -s  $dir/starship.toml ~/.config/starship.toml
-ln -s  $dir/dot_vimrc ~/.vimrc
-ln -s  $dir/ssh_config ~/.ssh/config
+ln -sf  $dir/dot_zshrc ~/.zshrc
+ln -sf  $dir/dot_bashrc ~/.bashrc
+ln -sf  $dir/config.fish ~/.config/fish/config.fish
+ln -sf  $dir/starship.toml ~/.config/starship.toml
+ln -sf  $dir/dot_vimrc ~/.vimrc
+ln -sf  $dir/ayu.vim ~/.vim/colors/ayu.vim
+ln -sf  $dir/solarized8.vim ~/.vim/colors/solarized8.vim
+ln -sf  $dir/space-vim-dark.vim ~/.vim/colors/space-vim-dark.vim
+ln -sf  $dir/ssh_config ~/.ssh/config
