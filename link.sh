1f5c8f08bdcd2ab9dca624b0a11d17846c7aefcc
Alex Salois <asalois.scholar@gmail.com>
2024-05-18 18:02:32 -0600

    Fixed a bad link to .gitconfig

--- link.sh
+++ link.sh
@@ -7,5 +7,6 @@ ln -sf  $dir/starship.toml ~/.config/starship.toml
 ln -sf  $dir/alacritty.yml ~/.config/alacritty/alacritty.yml
 ln -sf  $dir/alacritty.toml ~/.config/alacritty/alacritty.toml
 ln -sf  $dir/dot_vimrc ~/.vimrc
-ln -sf  $dir/dot_gotconfig ~/.gitconfig
+ln -sf  $dir/ayu.vim ~/.vim/colors/ayu.vim
+ln -sf  $dir/dot_gitconfig ~/.gitconfig
 ln -sf  $dir/ssh_config ~/.ssh/config
