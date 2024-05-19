057d2facf1f2dc3f67d61bfc82b281aee08c73ca
Alex Salois <asalois.scholar@gmail.com>
2024-05-18 18:35:12 -0600

    Changed the theme to Iceberg
    Also update link to link all *.vim file to ~/.vim/colors

--- link.sh
+++ link.sh
@@ -5,7 +5,5 @@ ln -sf  $dir/dot_bashrc ~/.bashrc
 ln -sf  $dir/config.fish ~/.config/fish/config.fish
 ln -sf  $dir/starship.toml ~/.config/starship.toml
 ln -sf  $dir/dot_vimrc ~/.vimrc
-ln -sf  $dir/ayu.vim ~/.vim/colors/ayu.vim
-ln -sf  $dir/solarized8.vim ~/.vim/colors/solarized8.vim
-ln -sf  $dir/space-vim-dark.vim ~/.vim/colors/space-vim-dark.vim
+ln -sf  $dir/*.vim ~/.vim/colors/
 ln -sf  $dir/ssh_config ~/.ssh/config
