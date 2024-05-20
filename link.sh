569acbb192b737dfe4fdce088a0c428f180c46f8
Alexander Salois <alexander.salois@montana.edu>
2024-05-20 09:07:19 -0600

    Added Iceberg theme to vim

--- link.sh
+++ link.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 ln -sf $HOME/dotfiles/dot_bashrc $HOME/.bashrc
+ln -sf $HOME/dotfiles/starship.toml $HOME/.config/starship.toml
 ln -sf $HOME/dotfiles/dot_gitconfig $HOME/.gitconfig
+ln -sf $HOME/dotfiles/tmux.conf $HOME/.tmux.conf
+ln -sf $HOME/dotfiles/dot_vimrc $HOME/.vimrc
+ln -sf $HOME/dotfiles/*.vim $HOME/.vim/colors/
 ln -sf $HOME/dotfiles/config.fish $HOME/.config/fish/config.fish
 
