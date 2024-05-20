2248e4b5dd4b56a35a39b78fa3db86184661a674
Alexander Salois <alexander.salois@montana.edu>
2024-05-20 08:42:46 -0600

    Added some files and the force option

--- link.sh
+++ link.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 dir=$(pwd)
-ln -s  $dir/dot_zshrc ~/.zshrc
-ln -s  $dir/dot_bashrc ~/.bashrc
-ln -s  $dir/starship.toml ~/.config/starship.toml
-ln -s  $dir/dot_vimrc ~/.vimrc
-ln -s  $dir/ssh_config ~/.ssh/config
+ln -sf  $dir/dot_zshrc ~/.zshrc
+ln -sf  $dir/dot_bashrc ~/.bashrc
+ln -sf  $dir/starship.toml ~/.config/starship.toml
+ln -sf  $dir/dot_vimrc ~/.vimrc
+ln -sf  $dir/*.vim ~/.vim/colors/
+ln -sf  $dir/tmux.conf ~/.tmux.conf
+ln -sf  $dir/ssh_config ~/.ssh/config
+ln -sf  $dir/dot_gitconfig ~/.gitconfig
