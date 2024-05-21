937b0696dd72499692ed494ab7b6d6c95474cc63
Alex Salois <asalois.scholar@gmail.com>
2024-05-20 21:18:05 -0600

    Added tmux conf for nice vim colors

--- link.sh
+++ link.sh
@@ -2,6 +2,7 @@
 dir=$(pwd)
 ln -sf  $dir/dot_zshrc ~/.zshrc
 ln -sf  $dir/dot_bashrc ~/.bashrc
+ln -sf  $dir/tmux.conf ~/.tmux.conf
 ln -sf  $dir/config.fish ~/.config/fish/config.fish
 ln -sf  $dir/starship.toml ~/.config/starship.toml
 ln -sf  $dir/dot_vimrc ~/.vimrc
