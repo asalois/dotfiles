05f40b80c6248ae18289686c6b1c5afcb30def0b
Alex Salois <asalois.scholar@gmail.com>
2024-05-26 20:04:14 -0600

    Fixes
    Added link to *.vim into ~/.vim/colors
    Removed alacrtitty.yml as Alacritty no longer using yaml
    Fixed bad link to gitconfig

--- link.sh
+++ link.sh
@@ -4,8 +4,8 @@ ln -sf  $dir/dot_zshrc ~/.zshrc
 ln -sf  $dir/dot_bashrc ~/.bashrc
 ln -sf  $dir/config.fish ~/.config/fish/config.fish
 ln -sf  $dir/starship.toml ~/.config/starship.toml
-ln -sf  $dir/alacritty.yml ~/.config/alacritty/alacritty.yml
 ln -sf  $dir/alacritty.toml ~/.config/alacritty/alacritty.toml
 ln -sf  $dir/dot_vimrc ~/.vimrc
-ln -sf  $dir/dot_gotconfig ~/.gitconfig
+ln -sf  $dir/*.vim ~/.vim/colors
+ln -sf  $dir/dot_gitconfig ~/.gitconfig
 ln -sf  $dir/ssh_config ~/.ssh/config
