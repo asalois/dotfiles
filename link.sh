51170db6efc2c057c30e60a54c24bbee1177fa1a
Alex Salois <asalois.scholar@gmail.com>
2026-01-20 19:53:47 -0700

    current state

--- link.sh
+++ link.sh
@@ -2,9 +2,9 @@
 dir=$(pwd)
 ln -sf  $dir/dot_zshrc ~/.zshrc
 ln -sf  $dir/dot_bashrc ~/.bashrc
-ln -sf  $dir/config.fish ~/.config/fish/config.fish
+#ln -sf  $dir/config.fish ~/.config/fish/config.fish
 ln -sf  $dir/starship.toml ~/.config/starship.toml
-ln -sf  $dir/alacritty.toml ~/.config/alacritty/alacritty.toml
+#ln -sf  $dir/alacritty.toml ~/.config/alacritty/alacritty.toml
 ln -sf  $dir/dot_vimrc ~/.vimrc
 ln -sf  $dir/*.vim ~/.vim/colors
 ln -sf  $dir/dot_gitconfig ~/.gitconfig
